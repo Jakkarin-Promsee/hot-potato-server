1feee29e4624867b5df423b8b2cbbe93e3a9095d
Night <jakkrinmes@gmail.com>
2026-04-04 16:48:32 +0700

    feat: init node js, express, ts, and .env

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+import userRoutes from "./routes/user.route";
+import { errorHandler } from "./middlewares/error.middleware";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ status: true });
+});
+
+app.use("/api/users", userRoutes);
+
+// Error Handler (must be last)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
